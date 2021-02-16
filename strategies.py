0543a0df4dbb4048fc85f4d987a83f62042ac48a
manuData <manuelvicarioperez@MacBook-Pro.local>
2021-02-16 23:39:44 +0100

    Incorporación de función para la creación de DataFrame de estrategias

--- strategies.py
+++ strategies.py
@@ -3,10 +3,10 @@
 
 1. Descargamos los datos de path donde estén los usuarios con perfiles públicos
 2. Generamos tabla con los usuarios seleccionados (TARGET)
-2. Generamos tabla con estrategias en función de las combinaciones f (stage que nos encontremos)
-3. Mapeo aleatoria de usuario-estrategia
-4. Asignación de fecha aleatoria en función de los días que querramos aplicar la estrategia
-5. Agrupación usuarios-estrategia para realización de post análisis
+3. Generamos tabla con estrategias en función de las combinaciones f (stage que nos encontremos)
+4. Mapeo aleatoria de usuario-estrategia
+5. Asignación de fecha aleatoria en función de los días que querramos aplicar la estrategia
+6. Agrupación usuarios-estrategia para realización de post análisis
 
 TO DO: Identificar las funciones que tiene que tener la clase
 '''
@@ -39,7 +39,7 @@ def load_data(path=None): # Importa los datos de un path concreto
 def df_rename_column(df): # Renombra la columna con "CSV"
     df.rename(columns={0:'CSV'},inplace=True)
 
-    
+# 2. Generamos tabla con los usuarios seleccionados (TARGET):
 def users_target_table(df): # Genera un DataFrame en el que cada csv es un row de datos
     rtdo = []
     for user_list in df.CSV:
@@ -51,12 +51,24 @@ def users_target_table(df): # Genera un DataFrame en el que cada csv es un row d
 
 # TO DO: def check_duplicates(df):
 
-
-# Lista para pasarle y que elimine los outliers:[73, 74]
+# 2.1 El output de la tabla es el DataFrame (df_users) deduplicado
 def remove_duplicates(df,index_remove=None): # Elimina del DataFrame los index que se pasen por parámetro (en forma de lista)
     df.drop(index_remove,inplace=True)
     df_users = df.reset_index().drop(columns='index')
     return df_users
+
+
+#3. Generamos tabla con estrategias en función de las combinaciones f (stage que nos encontremos)
+
+strategies_combinations_1 = ['follow & unfollow','comment','like','nombrar','reaccionar historias','ver historia']
+def strategies_generation(strategies_combinations=None): # Genera un DataFrame con las estrategias. TO DO: Si las combinaciones cambian.
+    global df_strategies
+    df_strategies = pd.DataFrame({'STRATEGIES':strategies_combinations})
+    return df_strategies
+
+
+
+
 
 
 # EJECUCIÓN DE FUNCIONES:
@@ -66,7 +78,11 @@ def remove_duplicates(df,index_remove=None): # Elimina del DataFrame los index q
 load_data(r'/Users/manuelvicarioperez/VICARIO_PROJECT/STRATEGY/test/TEST_PAU_ECHE/2_DATA_USERS_PUBLIC_TARGET')
 df_rename_column(data)
 users_target_table(data)
-remove_duplicates(df_users,[73,74])'''
+remove_duplicates(df_users,[73,74])
+strategies_generation(strategies_combinations_1)
+'''
+
+
 
 
 
